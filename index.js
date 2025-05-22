965c6d244875adfba353d259d26c6c32ef6be6eb
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-05-23 05:24:29 +0600

    udpdate issue

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const recipeCollection = client.db("recipeDB").collection('recipe')
 
